01f3899bb145e5b211075e58ee7071d42192dff4
stephanie mihut <stephanie.mihut@gmail.com>
2025-02-24 17:18:10 +0100

    mailer bug fixé ?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, unless: -> { controller_name == 'pages' && action_name == 'home' }
+  before_action :authenticate_user!, unless: -> {
+    (controller_name == 'pages' && action_name == 'home') ||
+    (controller_name == 'contacts' && action_name == 'create')
+  }
 end
+
