9cecd3450c0434d790c8be62f1c52cc2a27ad783
stephanie mihut <stephanie.mihut@gmail.com>
2025-02-26 18:40:08 +0100

    Correction du formulaire de contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,18 @@
 class ContactsController < ApplicationController
   def new
+    @contact = OpenStruct.new
   end
-  
-  def create
-    @contact = params.require(:contact).permit(:first_name, :last_name, :email, :message)
 
-    if @contact.present?
-      ContactMailer.contact_email(@contact).deliver_now
+  def create
+    contact_params = params.require(:contact).permit(:first_name, :last_name, :email, :message)
+
+    if contact_params.values.all?(&:present?) # Vérifie que tous les champs sont remplis
+      ContactMailer.contact_email(contact_params).deliver_now
       flash[:notice] = "Votre message a bien été envoyé."
       redirect_to root_path
     else
-      flash[:alert] = "Erreur lors de l'envoi du message."
-      render :new
+      flash[:alert] = "Erreur lors de l'envoi du message. Tous les champs sont requis."
+      render :new, status: :unprocessable_entity
     end
   end
 end
