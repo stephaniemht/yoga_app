306d0d2e3253ce16e701e7e387a152f1d45a5886
stephanie mihut <stephanie.mihut@gmail.com>
2025-02-27 08:07:35 +0100

    test formulaire en prod

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,12 @@
 class ContactsController < ApplicationController
   def new
-    @contact = OpenStruct.new
+    @contact = Contact.new
   end
 
   def create
-    contact_params = params.require(:contact).permit(:first_name, :last_name, :email, :message)
+    contact_params = params.require(:contact).permit(:first_name, :last_name, :email, :phone, :message)
 
-    if contact_params.values.all?(&:present?) # Vérifie que tous les champs sont remplis
+    if contact_params.values.all?(&:present?)
       ContactMailer.contact_email(contact_params).deliver_now
       flash[:notice] = "Votre message a bien été envoyé."
       redirect_to root_path
