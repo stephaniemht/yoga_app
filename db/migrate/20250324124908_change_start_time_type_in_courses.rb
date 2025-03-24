fbe380b5aff51479f9400ab70737109b9585e8b5
stephanie mihut <stephanie.mihut@gmail.com>
2025-03-24 14:01:17 +0100

    Fix conversion from time to timestamp using CURRENT_DATE

--- db/migrate/20250324124908_change_start_time_type_in_courses.rb
+++ db/migrate/20250324124908_change_start_time_type_in_courses.rb
@@ -1,5 +1,5 @@
 class ChangeStartTimeTypeInCourses < ActiveRecord::Migration[7.2]
   def change
-    change_column :courses, :start_time, 'timestamp USING start_time::timestamp'
+    change_column :courses, :start_time, 'timestamp USING (CURRENT_DATE + start_time)'
   end
 end
