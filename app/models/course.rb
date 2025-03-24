af9594e60a8d86c47d27f6a0f000d3ae0cc7e1e2
stephanie mihut <stephanie.mihut@gmail.com>
2025-03-24 19:35:23 +0100

    Remove custom start_time method to fix datetime_select bug

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,11 +3,4 @@ class Course < ApplicationRecord
   has_one_attached :photo
 
   validates :title, :start_time, :description, :location, :price, presence: true
-
-  def start_time
-    raw = super
-    return nil if raw.nil?
-    return raw if raw.is_a?(Time) || raw.is_a?(DateTime) || raw.is_a?(ActiveSupport::TimeWithZone)
-    Time.zone.parse(raw.to_s) rescue nil
-  end
 end
