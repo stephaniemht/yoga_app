4a172ad342b361d22c6f0794b0044cbee070dae6
stephanie mihut <stephanie.mihut@gmail.com>
2025-03-24 18:32:50 +0100

    Forcer redéploiement Heroku pour correction start_time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_24_102054) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_24_124908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,7 +52,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_24_102054) do
     t.datetime "updated_at", null: false
     t.string "duration"
     t.integer "price"
-    t.time "start_time"
+    t.datetime "start_time"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
