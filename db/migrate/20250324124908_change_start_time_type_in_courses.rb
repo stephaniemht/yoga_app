6cfbcbd5d0e67dbd5f244aea723489cb3e235055
stephanie mihut <stephanie.mihut@gmail.com>
2025-03-24 13:56:21 +0100

    Fix start_time type conversion with USING clause

--- db/migrate/20250324124908_change_start_time_type_in_courses.rb
+++ db/migrate/20250324124908_change_start_time_type_in_courses.rb
@@ -1,5 +1,5 @@
 class ChangeStartTimeTypeInCourses < ActiveRecord::Migration[7.2]
   def change
-    change_column :courses, :start_time, :datetime
+    change_column :courses, :start_time, 'timestamp USING start_time::timestamp'
   end
 end
