9e7cb179bf907ccd3b847da45300c3166be1e1bb
stephanie mihut <stephanie.mihut@gmail.com>
2025-03-24 11:53:42 +0100

    reglage formulaire 2

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,5 @@ class Course < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
 
-  validates :title, :description, :date, :location, :price, presence: true
+  validates :title, :start_time, :description, :date, :location, :price, presence: true
 end
